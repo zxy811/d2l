7f68e799acbdc8a57a7542f4bd29a9a38536af8d
zxy811 <1077731181@qq.com>
2026-03-09 22:14:21 +0800

    最新更改

--- test.py
+++ test.py
@@ -450,80 +450,6 @@ def evaluate_accuracy(net, data_iter):
     return metric[0] / metric[1]
 
 
-def train_epoch_ch3(net, train_iter, loss, updater):
-    if isinstance(net, torch.nn.Module):
-        net.train()
-    metric = Accumulator(3)
-    for X, y in train_iter:
-        y_hat = net(X)
-        # print(y_hat)
-        l = loss(y_hat, y)
-        # print(l)
-        if isinstance(updater, torch.optim.Optimizer):
-            updater.zero_grad()
-            l.mean().backward()
-            updater.step()
-        else:
-            l.sum().backward()
-            updater(X.shape[0])
-        metric.add(float(l.sum()), accuracy(y_hat, y), y.numel())
-        # print(metric)
-    return metric[0] / metric[2], metric[1] / metric[2]
-
-def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater):
-    # for X, y in train_iter:
-    #     print(X.shape, X.dtype, y.shape, y.dtype)
-    #     print(X)
-    #     print(y)
-    #     break
-    animator = Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=[0.3, 0.9],
-                        legend=['train loss', 'train acc', 'test acc'])
-    for epoch in range(num_epochs):
-        train_metrics = train_epoch_ch3(net, train_iter, loss, updater)
-        test_acc = evaluate_accuracy(net, test_iter)
-        print(train_metrics)
-        print(test_acc)
-        animator.add(epoch + 1, train_metrics + (test_acc,))
-    train_loss, train_acc = train_metrics
-    assert train_loss < 0.5, train_loss
-    assert train_acc <= 1 and train_acc > 0.7, train_acc
-    assert test_acc <= 1 and test_acc > 0.7, test_acc
-
-# # # def predict_ch3(net, test_iter, n=6):
-# # #     for X, y in test_iter:
-# # #         break
-# # #     trues = d2l.get_fashion_mnist_labels(y)
-# # #     preds = d2l.get_fashion_mnist_labels(net(X).argmax(axis=1))
-# # #     titles = [true +'\n' + pred for true, pred in zip(trues, preds)]
-# # #     d2l.show_images(
-# # #         X[0:n].reshape((n, 28, 28)), 1, n, titles=titles[0:n])
-# # # # predict_ch3(net, test_iter)
-# # # def updater(batch_size):
-# # #     return d2l.sgd([w, b], lr, batch_size)
-# # # # print(evaluate_accuracy(net, test_iter))
-# # # def main():
-# # #     # print(accuracy(y_hat
-
-# # #     train_ch3(net, train_iter, test_iter, cross_entropy, num_epochs, updater)
-# # # if __name__ == "__main__":
-# # #     lr=0.1
-# # #     num_epochs=10
-# # #     main()
-# # #     d2l.plt.show()
-
-# # import torch
-# # from torch import nn
-# # from d2l import torch as d2l
-# # batch_size = 256
-# # train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
-# # net=nn.Sequential(nn.Flatten(), nn.Linear(784, 10))
-# # def init_weights(m):
-# #     if type(m) == nn.Linear:
-# #         nn.init.normal_(m.weight, std=0.01)
-# # net.apply(init_weights)
-# # loss=nn.CrossEntropyLoss(reduction='none')
-# # trainer=torch.optim.SGD(net.parameters(), lr=0.1)
-# # num_epochs=10
 # def train_epoch_ch3(net, train_iter, loss, updater):
 #     if isinstance(net, torch.nn.Module):
 #         net.train()
@@ -562,75 +488,220 @@ def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater):
 #     assert train_loss < 0.5, train_loss
 #     assert train_acc <= 1 and train_acc > 0.7, train_acc
 #     assert test_acc <= 1 and test_acc > 0.7, test_acc
+
+# # # # def predict_ch3(net, test_iter, n=6):
+# # # #     for X, y in test_iter:
+# # # #         break
+# # # #     trues = d2l.get_fashion_mnist_labels(y)
+# # # #     preds = d2l.get_fashion_mnist_labels(net(X).argmax(axis=1))
+# # # #     titles = [true +'\n' + pred for true, pred in zip(trues, preds)]
+# # # #     d2l.show_images(
+# # # #         X[0:n].reshape((n, 28, 28)), 1, n, titles=titles[0:n])
+# # # # # predict_ch3(net, test_iter)
+# # # # def updater(batch_size):
+# # # #     return d2l.sgd([w, b], lr, batch_size)
+# # # # # print(evaluate_accuracy(net, test_iter))
+# # # # def main():
+# # # #     # print(accuracy(y_hat
+
+# # # #     train_ch3(net, train_iter, test_iter, cross_entropy, num_epochs, updater)
+# # # # if __name__ == "__main__":
+# # # #     lr=0.1
+# # # #     num_epochs=10
+# # # #     main()
+# # # #     d2l.plt.show()
+
+# # # import torch
+# # # from torch import nn
+# # # from d2l import torch as d2l
+# # # batch_size = 256
+# # # train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+# # # net=nn.Sequential(nn.Flatten(), nn.Linear(784, 10))
+# # # def init_weights(m):
+# # #     if type(m) == nn.Linear:
+# # #         nn.init.normal_(m.weight, std=0.01)
+# # # net.apply(init_weights)
+# # # loss=nn.CrossEntropyLoss(reduction='none')
+# # # trainer=torch.optim.SGD(net.parameters(), lr=0.1)
+# # # num_epochs=10
+# def train_epoch_ch3(net, train_iter, loss, updater):
+#     if isinstance(net, torch.nn.Module):
+#         net.train()
+#     metric = Accumulator(3)
+#     for X, y in train_iter:
+#         y_hat = net(X)
+#         # print(y_hat)
+#         l = loss(y_hat, y)
+#         # print(l)
+#         if isinstance(updater, torch.optim.Optimizer):
+#             updater.zero_grad()
+#             l.mean().backward()
+#             updater.step()
+#         else:
+#             l.sum().backward()
+#             updater(X.shape[0])
+#         metric.add(float(l.sum()), accuracy(y_hat, y), y.numel())
+#         # print(metric)
+#     return metric[0] / metric[2], metric[1] / metric[2]
+
+# def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater):
+#     # for X, y in train_iter:
+#     #     print(X.shape, X.dtype, y.shape, y.dtype)
+#     #     print(X)
+#     #     print(y)
+#     #     break
+#     animator = Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=[0.3, 0.9],
+#                         legend=['train loss', 'train acc', 'test acc'])
+#     for epoch in range(num_epochs):
+#         train_metrics = train_epoch_ch3(net, train_iter, loss, updater)
+#         test_acc = evaluate_accuracy(net, test_iter)
+#         print(train_metrics)
+#         print(test_acc)
+#         animator.add(epoch + 1, train_metrics + (test_acc,))
+#     train_loss, train_acc = train_metrics
+#     assert train_loss < 0.5, train_loss
+#     assert train_acc <= 1 and train_acc > 0.7, train_acc
+#     assert test_acc <= 1 and test_acc > 0.7, test_acc
+# # # def main():
+# # #     train_ch3(net, train_iter, test_iter, loss, num_epochs, trainer)
+
+# # # if __name__ == "__main__":
+# # #     main()
+# # #     d2l.plt.show()
+
+# # # import torch
+# # # from d2l import torch as d2l
+# # # x=torch.arange(-8.0, 8.0, 0.1, requires_grad=True)
+# # # y=torch.relu(x)
+# # # d2l.plot(x.detach().numpy(), y.detach().numpy(), 'x', 'relu(x)', figsize=(5, 2.5))
+# # # d2l.plt.show()
+# # # y.backward(torch.ones_like(x), retain_graph=True)
+# # # d2l.plot(x.detach().numpy(), x.grad.detach().numpy(), 'x', 'grad of relu(x)', figsize=(5, 2.5))
+# # # d2l.plt.show()
+
+# # # import torch
+# # # from d2l import torch as d2l
+# # # x=torch.arange(-8.0, 8.0, 0.1, requires_grad=True)
+# # # y=torch.tanh(x)
+# # # d2l.plot(x.detach().numpy(), y.detach().numpy(), 'x', 'sigmoid(x)', figsize=(5, 2.5))
+# # # d2l.plt.show()
+# # # y.backward(torch.ones_like(x), retain_graph=True)
+# # # d2l.plot(x.detach().numpy(), x.grad.detach().numpy(), 'x', 'grad of sigmoid(x)', figsize=(5, 2.5))
+# # # d2l.plt.show()
+
+# # import torch
+# # from torch import nn
+# # from d2l import torch as d2l
+# # batch_size = 256
+# # train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+# # num_inputs, num_outputs, num_hiddens = 784, 10, 256
+# # W1 = nn.Parameter(torch.randn(num_inputs, num_hiddens, requires_grad=True) * 0.01)
+# # b1 = nn.Parameter(torch.zeros(num_hiddens, requires_grad=True))
+# # W2 = nn.Parameter(torch.randn(num_hiddens, num_outputs, requires_grad=True) * 0.01)
+# # b2 = nn.Parameter(torch.zeros(num_outputs, requires_grad=True))
+# # params = [W1, b1, W2, b2]
+# # def relu(X):
+# #     a = torch.zeros_like(X)
+# #     return torch.max(X, a)
+# # def net(X):
+# #     X = X.reshape((-1, W1.shape[0]))
+# #     H = relu(X @ W1 + b1)
+# #     return H @ W2 + b2
+# # loss=nn.CrossEntropyLoss(reduction='none')
+# # trainer=torch.optim.SGD(params, lr=0.1)
+# # num_epochs=10
 # # def main():
 # #     train_ch3(net, train_iter, test_iter, loss, num_epochs, trainer)
-
+# #     d2l.plt.show()
 # # if __name__ == "__main__":
 # #     main()
-# #     d2l.plt.show()
 
-# # import torch
-# # from d2l import torch as d2l
-# # x=torch.arange(-8.0, 8.0, 0.1, requires_grad=True)
-# # y=torch.relu(x)
-# # d2l.plot(x.detach().numpy(), y.detach().numpy(), 'x', 'relu(x)', figsize=(5, 2.5))
-# # d2l.plt.show()
-# # y.backward(torch.ones_like(x), retain_graph=True)
-# # d2l.plot(x.detach().numpy(), x.grad.detach().numpy(), 'x', 'grad of relu(x)', figsize=(5, 2.5))
-# # d2l.plt.show()
-
-# # import torch
-# # from d2l import torch as d2l
-# # x=torch.arange(-8.0, 8.0, 0.1, requires_grad=True)
-# # y=torch.tanh(x)
-# # d2l.plot(x.detach().numpy(), y.detach().numpy(), 'x', 'sigmoid(x)', figsize=(5, 2.5))
-# # d2l.plt.show()
-# # y.backward(torch.ones_like(x), retain_graph=True)
-# # d2l.plot(x.detach().numpy(), x.grad.detach().numpy(), 'x', 'grad of sigmoid(x)', figsize=(5, 2.5))
-# # d2l.plt.show()
 
 # import torch
 # from torch import nn
 # from d2l import torch as d2l
-# batch_size = 256
-# train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
-# num_inputs, num_outputs, num_hiddens = 784, 10, 256
-# W1 = nn.Parameter(torch.randn(num_inputs, num_hiddens, requires_grad=True) * 0.01)
-# b1 = nn.Parameter(torch.zeros(num_hiddens, requires_grad=True))
-# W2 = nn.Parameter(torch.randn(num_hiddens, num_outputs, requires_grad=True) * 0.01)
-# b2 = nn.Parameter(torch.zeros(num_outputs, requires_grad=True))
-# params = [W1, b1, W2, b2]
-# def relu(X):
-#     a = torch.zeros_like(X)
-#     return torch.max(X, a)
-# def net(X):
-#     X = X.reshape((-1, W1.shape[0]))
-#     H = relu(X @ W1 + b1)
-#     return H @ W2 + b2
+# net = nn.Sequential(nn.Flatten(), nn.Linear(784, 256),nn.ReLU(),nn.Linear(256, 10))
+# def init_weights(m):
+#     if type(m) == nn.Linear:
+#         nn.init.normal_(m.weight, std=0.01)
+# net.apply(init_weights)
+# batch_size,lr,num_epochs=256,0.1,10
 # loss=nn.CrossEntropyLoss(reduction='none')
-# trainer=torch.optim.SGD(params, lr=0.1)
-# num_epochs=10
+# trainer=torch.optim.SGD(net.parameters(), lr=lr)
+# train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
 # def main():
 #     train_ch3(net, train_iter, test_iter, loss, num_epochs, trainer)
 #     d2l.plt.show()
 # if __name__ == "__main__":
 #     main()
 
-
-import torch
+import numpy as np
+import math
 from torch import nn
+import torch
 from d2l import torch as d2l
-net = nn.Sequential(nn.Flatten(), nn.Linear(784, 256),nn.ReLU(),nn.Linear(256, 10))
-def init_weights(m):
-    if type(m) == nn.Linear:
-        nn.init.normal_(m.weight, std=0.01)
-net.apply(init_weights)
-batch_size,lr,num_epochs=256,0.1,10
-loss=nn.CrossEntropyLoss(reduction='none')
-trainer=torch.optim.SGD(net.parameters(), lr=lr)
-train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+max_degree=20
+n_train, n_test = 100, 100
+true_w = np.zeros(max_degree)
+true_w[0:4] = np.array([5, 1.2, -3.4, 5.6])
+features = np.random.normal(size=(n_train + n_test, 1))
+np.random.shuffle(features)
+poly_features = np.power(features, np.arange(max_degree).reshape(1, -1))
+for i in range(max_degree):
+    poly_features[:, i] /= math.gamma(i + 1)
+labels = np.dot(poly_features, true_w)
+labels += np.random.normal(scale=0.1, size=labels.shape)
+# print(features[:2])
+# print(poly_features[:2])
+# print(labels[:2])
+true_w,features,poly_features,labels = [torch.tensor(x, dtype=
+    torch.float32) for x in [true_w, features, poly_features, labels]]
+# print(features[:2])
+# print(poly_features[:2])
+# print(labels[:2])
+def evaluate_loss(net, data_iter, loss):
+    """评估给定数据集上模型的损失"""
+    metric = d2l.Accumulator(2)
+    for X, y in data_iter:
+        out = net(X)
+        y = y.reshape(out.shape)
+        l = loss(out, y)
+        metric.add(l.sum(), l.numel())
+    return metric[0] / metric[1]
+def train(train_features, test_features, train_labels, test_labels,
+          num_epochs=400):
+    loss = nn.MSELoss(reduction='none')
+    # print(train_features.shape, test_features.shape, train_labels.shape, test_labels.shape)
+    # print(train_features[:2])
+    # print(test_features[:2])
+    # print(train_labels[:2])
+    # print(test_labels[:2])
+    # input_shape = train_features.shape[-1]
+    # print("--------------------------------")
+    # print(input_shape)
+    # 不设置偏置，因为我们已经在多项式中实现了它
+    net = nn.Sequential(nn.Linear(input_shape, 1, bias=False))
+    batch_size = min(10, train_labels.shape[0])
+    train_iter = d2l.load_array((train_features, train_labels.reshape(-1,1)),
+                                batch_size)
+    # print(train_labels)
+    # print(train_labels.reshape(-1,1))
+    test_iter = d2l.load_array((test_features, test_labels.reshape(-1,1)),
+                               batch_size, is_train=False)
+    trainer = torch.optim.SGD(net.parameters(), lr=0.01)
+    animator = d2l.Animator(xlabel='epoch', ylabel='loss', yscale='log',
+                            xlim=[1, num_epochs], ylim=[1e-3, 1e2],
+                            legend=['train', 'test'])
+    for epoch in range(num_epochs):
+        train_epoch_ch3(net, train_iter, loss, trainer)
+        if epoch == 0 or (epoch + 1) % 20 == 0:
+            animator.add(epoch + 1, (evaluate_loss(net, train_iter, loss),
+                                     evaluate_loss(net, test_iter, loss)))
+    print('weight:', net[0].weight.data.numpy())
+
 def main():
-    train_ch3(net, train_iter, test_iter, loss, num_epochs, trainer)
-    d2l.plt.show()
+    train(poly_features[:n_train, :4], poly_features[n_train:, :4],
+      labels[:n_train], labels[n_train:])
+    # d2l.plt.show()
 if __name__ == "__main__":
     main()
